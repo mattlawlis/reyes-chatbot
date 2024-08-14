39419247e045940e27aac5714283ca277a812f3d
mattlawlis <89171568+mattlawlis@users.noreply.github.com>
2024-08-14 02:43:19 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ fontFamily: 'ReyesLawLight, sans-serif' }}>{children}</body>
     </html>
   );
 }
